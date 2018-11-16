5d9072539c66d90b3b2db9763e8a80a024cb76c6
NoorSulaiman <noor.s.suliman@gmail.com>
2018-11-15 21:37:11 -0500

    react router setup with home page

---
+++ src/components/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+    state = {}
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default HomePage;
